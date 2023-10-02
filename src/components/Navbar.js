d101310d53c19a978e9d0c623bc0e76a9e7e8674
Priya <priyapiyuse@gmail.com>
2023-10-02 04:40:45 -0700

    second push

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React from 'react'
 import Logo from '../assets/monkey.jpeg';
+import { Link } from "react-router-dom";
+import "../styles/Navbar.css";
+import ReorderIcon from '@mui/icons-material/Reorder';
 function Navbar() {
   return (
     <div className="navbar">
         <div className='leftSide'> 
         <img src={Logo} />
         </div>
-        <div className='rightSide'> </div>
+        <div className='rightSide'>
+          <Link to='/'>Home</Link>  
+          <Link to='/menu'>Menu</Link>  
+          <Link to='/about'>About</Link>  
+          <Link to='/contact'>Contact</Link>  
+          <button>
+             <ReorderIcon />
+          </button>
+         </div>
       
     </div>
   )
