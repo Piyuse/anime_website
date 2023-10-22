0466431efc61496da4d2c041b5fb547b5d53cd65
Priya <priyapiyuse@gmail.com>
2023-10-21 23:59:26 -0700

    hiddenLink commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,12 @@ function Navbar() {
     <div className="navbar">
         <div className='leftSide'> 
         <img src={Logo} />
+        <div className="hiddenLinks"> 
+          <Link to='/'>Home</Link>  
+          <Link to='/menu'>Menu</Link>  
+          <Link to='/about'>About</Link>  
+          <Link to='/contact'>Contact</Link> 
+        </div>
         </div>
         <div className='rightSide'>
           <Link to='/'>Home</Link>  
